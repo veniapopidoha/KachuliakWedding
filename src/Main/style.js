f24b08307601118796d0d33c8922c378fe98fc68
Venia <dev14@globalpartnersoft.com>
2023-06-12 15:29:33 +0300

    new blocks

--- src/Main/style.js
+++ src/Main/style.js
@@ -36,7 +36,50 @@ export const Text2 = styled.h2`
 export const InvitationBlock = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   img {
     width: 50vw;
   }
+`;
+
+export const Text3 = styled.h2`
+  font-family: 'Playfair Display', serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: #ffffff;
+`;
+
+export const Text4 = styled.h2`
+  font-family: 'PT Sans', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 150%;
+  text-align: justify;
+  width: 80%;
+
+  color: #ffffff;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Text5 = styled.h2`
+  font-family: 'Poiret One';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 35px;
+  line-height: 150%;
+  width: 70vw;
+
+  color: #ffffff;
 `;
