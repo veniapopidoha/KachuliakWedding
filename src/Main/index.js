42780d06c3722c1ed394fef4c95474339d69fbc2
Venia <dev14@globalpartnersoft.com>
2023-06-12 13:20:57 +0300

    first commit

---
+++ src/Main/index.js
@@ -0,0 +1,9 @@
+import { Back } from './style'
+import back from '../img/back.png'
+
+export const Main = () => {
+  return <div>
+    <Back src={back}/>
+    <div></div>
+  </div>
+}
---
+++ src/Main/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Back = styled.img`
+  height: 100vh;
+  width: 100vw;
+`;
