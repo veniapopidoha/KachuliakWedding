6f26870de66b66ad33b18b3930c6fb2c926d71f5
Venia <dev14@globalpartnersoft.com>
2023-06-12 14:42:33 +0300

    block 2

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,27 @@
-import { Back } from './style'
-import back from '../img/back.png'
+import { Back, InvitationBlock, Text, Text2 } from './style';
+import back from '../img/back.png';
+import karina from '../img/karina.png';
 
 export const Main = () => {
-  return <div>
-    <Back src={back}/>
-    <div></div>
-  </div>
-}
+  return (
+    <div>
+      <Back src={back} />
+      <InvitationBlock>
+        <div>
+          <Text>Ми щиро запрошуємо вас на святкування нашого дня весілля.</Text>
+          <br />
+          <Text>28 липня 2023 12:00</Text>
+          <Text2>
+            весілля відбудеться на базі{' '}
+            <a href='https://liteplo.com.ua' target='_blank'>
+              liteplo.com.ua
+            </a>
+          </Text2>
+        </div>
+        <div>
+          <img src={karina} />
+        </div>
+      </InvitationBlock>
+    </div>
+  );
+};
--- src/Main/style.js
+++ src/Main/style.js
@@ -4,3 +4,39 @@ export const Back = styled.img`
   height: 100vh;
   width: 100vw;
 `;
+
+export const Text = styled.h1`
+  font-family: 'Poiret One', cursive;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 38px;
+  line-height: 44px;
+  text-align: center;
+  letter-spacing: 0.08em;
+  text-transform: uppercase;
+
+  color: #ffffff;
+`;
+
+export const Text2 = styled.h2`
+  font-family: 'Poiret One', cursive;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 26px;
+  text-align: center;
+  letter-spacing: 0.06em;
+  text-transform: uppercase;
+  color: #ffffff;
+  a {
+    color: #fff;
+  }
+`;
+
+export const InvitationBlock = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 50vw;
+  }
+`;
